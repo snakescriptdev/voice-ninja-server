60cf94e477a25bd8882732e49bd82bdbcc33fa1b
abhishekkumar-snakescript <abhishekkumar@snakescript.com>
2025-09-02 17:57:47 +0530

    updated voice retell

--- scripts/elevenlab_llm_models_add.py
+++ scripts/elevenlab_llm_models_add.py
@@ -1,6 +1,9 @@
 import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_URL = os.getenv("DB_URL", None)
 if not DB_URL:
