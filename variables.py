c8c980c8a7d5acf0cb30ff10414fc18d8a220aad
Jaspreet Kaur <jaspreet@snakescript.com>
2026-01-06 12:39:44 +0530

    update response structure in user login and registration

---
+++ variables.py
@@ -0,0 +1,24 @@
+# Response Keys
+STATUS = 'status'
+ERROR = 'error'
+MESSAGE = 'message'
+DATA = 'data'
+ACCESS_TOKEN = 'access_token'
+USER = 'user'
+
+# Status Values
+SUCCESS = 'success'
+FAILED = 'error'
+
+# HTTP Status Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_400_BAD_REQUEST = 400
+HTTP_401_UNAUTHORIZED = 401
+HTTP_403_FORBIDDEN = 403
+HTTP_404_NOT_FOUND = 404
+HTTP_500_INTERNAL_SERVER_ERROR = 500
+
+# User Roles
+ROLE_USER = 'USER'
+ROLE_ADMIN = 'ADMIN'
