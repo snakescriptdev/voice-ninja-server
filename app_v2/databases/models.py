305aba407251df6b1cc59721a4ea4815e594e4c4
vikram <vikram@snakescript.com>
2026-01-28 18:31:18 +0530

    added the bridge model for lang and agent

--- app_v2/databases/models.py
+++ app_v2/databases/models.py
@@ -302,6 +302,27 @@ class AgentAIModelBridge(Base):
 
     )
 
+class AgentLanguageBridge(Base):
+
+    __tablename__ = "agent_language_bridge"
+
+
+    id: Mapped[int] = mapped_column(Integer, primary_key= True, index= True,autoincrement=True)
+
+    agent_id: Mapped[int] = mapped_column(Integer,ForeignKey("agents.id"))
+    lang_id: Mapped[int]  = mapped_column(Integer,ForeignKey("languages.id"))
+
+    __table_args__ = (
+                    UniqueConstraint("agent_id","lang_id",name="uq_lang_bridge_agent_id_lang_id"),
+                    Index("ix_agent_lang_bridge_agent_id","agent_id"),
+                    Index("ix_agent_llang_bridge_lang_id","lang_id")
+        
+    )
+
+    agent = relationship("AgentModel",back_populates="agent_languages")
+    language = relationship("LanguageModel",back_populates="agent_languages")
+
+
 
 
 Base.metadata.create_all(engine)
