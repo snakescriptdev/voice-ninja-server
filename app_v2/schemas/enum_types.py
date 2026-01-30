22699ef5d1ab0618d2ef16b510667e9afdac8d89
vikram <vikram@snakescript.com>
2026-01-30 18:19:52 +0530

    added schema 4 functions, jwt-expire session increased, updated bug in verify otp

---
+++ app_v2/schemas/enum_types.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class RequestMethodEnum(str,Enum):
+    get = "get"
+    post = "post"
+    put = "put"
+    delete = "delete"
+    patch = "patch"
+
+class UseCases(str,Enum):
+    email_assistant = "email_assistant"
+    task_execution = "task_execution"
+    system_assistant = "system_assistant"
+    knowledge_lookup = "knowledge_lookup"
+    customer_support = "customer_support"
+    custom = "custom"
+
+
+class Capebilites(str,Enum):
+    email_integration = "email_integration"
+    calendar_management = "calendar_management"
+    knowledge_base = "knowledge_base"
+    api_integration = "api_integration"
+
+class ResponseStyleEnum(str, Enum):
+    professional = "professional"
+    friendly = "friendly"
+    casual = "casual"
