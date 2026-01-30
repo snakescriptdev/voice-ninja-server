84b99d4ebfb1de3deac2fc7a11a1764c799b14d6
vikram <vikram@snakescript.com>
2026-01-30 18:19:52 +0530

    added schema 4 functions, jwt-expire session increased, updated bug in verify otp

--- app_v2/core/config.py
+++ app_v2/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     # Security
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 24 * 60  #1d 
     
     # Database Configuration
     DB_URL: str
