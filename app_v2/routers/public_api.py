bf163885dcea93e2fe28a6694bc99a71e815a373
priyanshiDev1301 <146160120+priyanshiDev1301@users.noreply.github.com>
2026-08-18 16:03:47 +0530

    BE-fixes (#70)

--- app_v2/routers/public_api.py
+++ app_v2/routers/public_api.py
@@ -1418,7 +1418,7 @@ async def list_widgets(
                 shareable_link=f"{base_url}/api/v2/widget/preview/{wa.public_id}",
                 is_enabled=wa.is_enabled,
                 created_at=wa.created_at,
-                updated_at=wa.modified_at,
+                updated_at=wa.modified_at or wa.created_at,
                 agent_id=wa.agent_id,
                 agent_name=wa.agent.agent_name,
                 primary_color=wa.primary_color,
