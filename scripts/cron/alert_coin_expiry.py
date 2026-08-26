773b0069a7ceb55ef213886923bb92e13a18d8ca
Priyanshi Soni <priyanshi@wope.ai>
2026-08-26 17:56:43 +0530

    fix

--- scripts/cron/alert_coin_expiry.py
+++ scripts/cron/alert_coin_expiry.py
@@ -7,7 +7,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-SMTP_SERVER = "smtp.gmail.com"
+SMTP_SERVER = ""
 SMTP_PORT = 587
 
 SMTP_USERNAME = ""
